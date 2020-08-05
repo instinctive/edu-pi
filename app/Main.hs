a548e595e938b6ae6edb1ffca107b6cbff0e6ae3
Charles Martin <charles.martin@gmail.com>
2020-08-05 16:22:15 -0700

    Refactor conditional, add strictness annotation

--- app/Main.hs
+++ app/Main.hs
@@ -11,11 +11,11 @@ main = do
     calc <- parse <$> getArgs
     reporter <- mkReporter
 
-    let go z n (Calc m) = do
+    let go !z !n (Calc m) = do
             (pi, calc') <- m
-            if z == n then do
-                reporter n pi
-                go (z*2) (n+1) calc'
-            else go z (n+1) calc'
+            z' <- if z == n
+                then reporter n pi >> pure (z*2)
+                else pure z
+            go z' (n+1) calc'
 
-    go 1 0 calc
+    go (1::Int) 0 calc
